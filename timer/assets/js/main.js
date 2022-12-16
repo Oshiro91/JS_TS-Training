e91422eecbf83dd8cb469b0b643eec1dbdbbad8f
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2022-12-16 15:14:24 -0300

    V1 (funtional)

--- timer/assets/js/main.js
+++ timer/assets/js/main.js
@@ -1,16 +1,91 @@
-const clock = document.querySelector('.clock');
+let clockHour, clockMin, clockSeg
+clockHour = clockMin = clockSeg = "00"
+const clock = document.getElementById("clock");
 const init = document.querySelector('.init');
 const pause = document.querySelector('.pause');
 const reset = document.querySelector('.reset');
+const test = document.querySelector('.test');
+var addSeg
+refreshClock();
 
-init.addEventListener('click',function(oEvent){
-    alert("Start button has been pressed")
+function refreshClock() {
+    clock.innerHTML = `${clockHour}:${clockMin}:${clockSeg}`
+};
+
+init.addEventListener('click', function (oEvent) {
+    clock.classList.remove("paused");
+    addSeg = setInterval(() => {
+        addTime("seg")
+    }, 1000);
 });
 
-pause.addEventListener('click',function(oEvent){
-    alert("Pause button has been pressed")
+pause.addEventListener('click', function (oEvent) {
+    clearInterval(addSeg)
+    clock.classList.add("paused");
 });
 
-reset.addEventListener('click',function(oEvent){
-    alert("Reset button has been pressed")
+reset.addEventListener('click', function (oEvent) {
+    restartClock();
+    clock.classList.remove("paused");
 });
+
+test.addEventListener('click', function (oEvent) {
+    addTime("seg")
+});
+
+function addTime(gran) {
+    switch (gran) {
+        case "seg":
+            if (clockSeg < 59) {
+                if (clockSeg<9) {
+                    clockSeg++
+                    clockSeg = `0${clockSeg}`
+                } else {
+                    clockSeg++
+                }
+                refreshClock()
+            } else {
+                clockSeg = "00"
+                addTime("min")
+            }
+            break;
+        case "min":
+            if (clockMin < 59) {
+                if (clockMin<9) {
+                    clockMin++
+                    clockMin = `0${clockMin}`
+                } else {
+                    clockMin++
+                }
+                refreshClock()
+            } else {
+                clockMin = "00"
+                addTime("hour")
+            }
+        break;
+
+        case "hour":
+            if (clockHour < 23) {
+                if (clockHour<9) {
+                    clockHour++
+                    clockHour = `0${clockHour}`
+                } else {
+                    clockHour++
+                }
+                refreshClock()
+            } else {
+                clockHour = "00"
+                refreshClock()
+            }
+        break
+    
+        default:
+            break;
+    }
+    
+}
+
+function restartClock(){
+    clockHour = clockMin = clockSeg = "00"
+    refreshClock();
+}
