389a59399888dd2fe2fc50a1eb43b5ced018c526
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2022-12-19 14:07:14 -0300

    v1Final

--- todo_list/assets/js/main.js
+++ todo_list/assets/js/main.js
@@ -1,6 +1,7 @@
 const newChoreInput = document.getElementById("inpt_NewChore");
 const bt_addGoal = document.getElementById("bt_addNewChore");
 const choreList = document.getElementById("choresList");
+onInit();
 
 bt_addGoal.addEventListener('click', function (oEvent) {
     if (newChoreInput.value) {
@@ -17,15 +18,32 @@ newChoreInput.addEventListener('keypress', function (oEvent) {
 choreList.addEventListener('click', function(oEvent){
     if (oEvent.target.getInnerHTML() == "Delete") {
         oEvent.target.parentElement.remove();
+        saveChoreList();
     }
 })
+
+function onInit () {
+   const localChore = onGetLocalChores();
+   onPopulateChoreList(localChore);
+};
+
+function onGetLocalChores () {
+    return JSON.parse(localStorage.getItem("ChoresList"))
+};
+
+function onPopulateChoreList(localChore) {
+    localChore.forEach(element => {
+        createLiChore(element)
+    });
+};
 
 function createLiChore(choreText) {
     let newChore = document.createElement('li');
     newChore.innerHTML = choreText;
     newChore.innerHTML += "    "
     newChore.appendChild(createDeleteButton());
-    choreList.appendChild(newChore);    
+    choreList.appendChild(newChore);
+    saveChoreList();
     cleanChoreInput();
 };
 
@@ -37,4 +55,17 @@ function createDeleteButton() {
     let delButon = document.createElement('button')
     delButon.innerHTML = "Delete"
     return delButon
+};
+
+function saveChoreList(){
+    const choreNum = choreList.querySelectorAll('li');
+    let aListChore = [];
+    for (let i = 0; i < choreNum.length; i++) {
+        const chore = choreNum[i].firstChild.textContent.trim()
+        aListChore.push(chore)        
+    }
+    console.log(aListChore)
+    aListChore = JSON.stringify(aListChore)
+    console.log(aListChore)
+    localStorage.setItem("ChoresList",aListChore)
 };
