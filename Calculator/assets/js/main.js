d11bc0518a2255a751e07c1e4faab334d03ac785
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2022-12-29 11:29:14 -0300

    show on display

--- Calculator/assets/js/main.js
+++ Calculator/assets/js/main.js
@@ -1,36 +1,19 @@
-// Capturar o evento de submit do formulário
-const form = document.querySelector("#formIMC");
+function Calculadora (){
+    this.display = document.querySelector('.display')
 
-form.addEventListener("submit", function (Evt) {
-    Evt.preventDefault();
-    let peso,altura,msg;
-    peso = document.getElementById("peso").value;
-    altura = document.getElementById("altura").value ;   
-    if (pesoValid() && alturaValid()) {
-        msg = "Seu IMC é de: "+IMCCalc(peso,altura);
-        document.querySelector("#resultado").innerHTML = `<p>${msg}</p>`;
+    this.captureClicks = () => {
+        document.addEventListener('click', oEvent => {
+            const el = oEvent.target;
+            el.classList.contains('btn-num') ? this.addNumDisplay(el) : false 
+        })
     }
-});
 
-const IMCCalc = (peso,altura) => {
-    let IMC = peso/(altura^2);
-    return IMC;
-};
+    this.addNumDisplay = (el) => this.display.value += el.innerText
 
-const pesoValid = function () {
-    if (Number(document.getElementById("peso").value) != document.getElementById("peso").value) {
-        alert("Peso Inválido")
-        return false
-    } else {
-        return true
-    } 
-};
+    this.init = () => {
+        this.captureClicks()
+    }
+}
 
-const alturaValid = function () {
-    if (Number(document.getElementById("altura").value) != document.getElementById("altura").value) {
-        alert("Altura Inválida")
-        return false
-    } else {
-        return true
-    } 
-};
+const calculadora = new Calculadora();
+calculadora.init();
